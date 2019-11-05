d5810154b18f35fa7390eca0656e67d206b8952e
LiangYuxuan <lyx@lyx.es>
2019-11-05 16:26:51 +0800

    EFL: following update * support CoD:MW * support bnet d/c

--- Modules/Standalones/EnhancedFriendsList.lua
+++ Modules/Standalones/EnhancedFriendsList.lua
@@ -9,6 +9,7 @@ local EFL = R:NewModule('EnhancedFriendsList', 'AceEvent-3.0', 'AceHook-3.0', 'A
 local format, pairs, strsplit, time, unpack = format, pairs, strsplit, time, unpack
 
 -- WoW API / Variables
+local BNConnected = BNConnected
 local BNGetFriendInfo = BNGetFriendInfo
 local BNGetGameAccountInfo = BNGetGameAccountInfo
 local C_BattleNet_GetFriendAccountInfo = C_BattleNet and C_BattleNet.GetFriendAccountInfo
@@ -42,6 +43,8 @@ local ONE_HOUR = 60 * ONE_MINUTE
 local ONE_DAY = 24 * ONE_HOUR
 local ONE_MONTH = 30 * ONE_DAY
 local ONE_YEAR = 12 * ONE_MONTH
+
+local isBNConnected = BNConnected()
 
 EFL.Icons = {
     Game = {
@@ -173,6 +176,16 @@ EFL.Icons = {
             Flat = MediaPath..[[GameIcons\Launcher\COD4]],
             Gloss = MediaPath..[[GameIcons\Launcher\COD4]],
             Launcher = MediaPath..[[GameIcons\Launcher\COD4]],
+        },
+        ODIN = {
+            Name = "使命召唤：现代战争",
+            Order = 14,
+            Color = 'FFFFFF',
+            Default = MediaPath..[[GameIcons\Launcher\CODMW]],
+            BlizzardChat = MediaPath..[[GameIcons\Launcher\CODMW]],
+            Flat = MediaPath..[[GameIcons\Launcher\CODMW]],
+            Gloss = MediaPath..[[GameIcons\Launcher\CODMW]],
+            Launcher = MediaPath..[[GameIcons\Launcher\CODMW]],
         },
     },
     Status = {
@@ -364,7 +377,7 @@ function EFL:UpdateFriends(button)
             nameText = info.name
         end
         button.status:SetTexture(EFL.Icons.Status[status][E.db.RhythmBox.EnhancedFriendsList.StatusIconPack])
-    elseif button.buttonType == FRIENDS_BUTTON_TYPE_BNET then
+    elseif button.buttonType == FRIENDS_BUTTON_TYPE_BNET and isBNConnected then
         local info = EFL:GetBattleNetInfo(button.id)
         if info then
             nameText = info.accountName
@@ -391,18 +404,19 @@ function EFL:UpdateFriends(button)
                     local faction = info.gameAccountInfo.factionName
                     button.gameIcon:SetTexture(faction and EFL.Icons.Game[faction][E.db.RhythmBox.EnhancedFriendsList[faction]] or EFL.Icons.Game.Neutral.Launcher)
                 else
+                    if not EFL.Icons.Game[client] then client = 'BSAp' end
                     nameText = format('|cFF%s%s|r', EFL.Icons.Game[client].Color or 'FFFFFF', nameText)
                     button.gameIcon:SetTexture(EFL.Icons.Game[client][E.db.RhythmBox.EnhancedFriendsList[client]])
                 end
 
                 button.gameIcon:SetTexCoord(0, 1, 0, 1)
-                button.gameIcon:SetDrawLayer('OVERLAY')
+                button.gameIcon:SetDrawLayer('ARTWORK')
                 button.gameIcon:SetAlpha(1)
             else
                 local lastOnline = info.lastOnlineTime
                 infoText = (not lastOnline or lastOnline == 0 or time() - lastOnline >= ONE_YEAR) and FRIENDS_LIST_OFFLINE or format(BNET_LAST_ONLINE_TIME, FriendsFrame_GetLastOnline(lastOnline))
             end
-			button.status:SetTexture(EFL.Icons.Status[status][E.db.RhythmBox.EnhancedFriendsList.StatusIconPack])
+            button.status:SetTexture(EFL.Icons.Status[status][E.db.RhythmBox.EnhancedFriendsList.StatusIconPack])
         end
     end
 
@@ -452,6 +466,10 @@ function EFL:UpdateFriends(button)
     end
 end
 
+function EFL:HandleBN()
+    isBNConnected = BNConnected()
+end
+
 P["RhythmBox"]["EnhancedFriendsList"] = {
     ["Enable"] = true,
     ["NameFont"] = E.db.general.font,
@@ -465,7 +483,7 @@ P["RhythmBox"]["EnhancedFriendsList"] = {
     ["ShowStatusBackground"] = false,
     ["Texture"] = "Solid",
 }
-for _, GameIcon in pairs({'Alliance', 'Horde', 'Neutral', 'D3', 'WTCG', 'S1', 'S2', 'App', 'BSAp', 'Hero', 'Pro', 'DST2', 'VIPR' }) do
+for GameIcon in pairs(EFL.Icons.Game) do
     P["RhythmBox"]["EnhancedFriendsList"][GameIcon] = 'Launcher'
 end
 
@@ -634,6 +652,9 @@ end
 tinsert(R.Config, FriendsListOptions)
 
 function EFL:Initialize()
+    EFL:RegisterEvent('BN_CONNECTED', 'HandleBN')
+    EFL:RegisterEvent('BN_DISCONNECTED', 'HandleBN')
+
     self:SecureHook('FriendsFrame_UpdateFriendButton', 'UpdateFriends')
 end
 
