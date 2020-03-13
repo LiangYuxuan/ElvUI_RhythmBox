5a0881a45bf6116ce700cbec7d1d3c44a643aa80
LiangYuxuan <lyx@lyx.es>
2020-03-13 16:15:06 +0800

    injections: introducing MailCommander, stops it from rebinding SHIFT-P

---
+++ Modules/Injections/MailCommander.lua
@@ -0,0 +1,25 @@
+local R, E, L, V, P, G = unpack(select(2, ...))
+
+if R.Classic then return end
+
+local RI = R:GetModule('Injections')
+
+-- Lua functions
+
+-- WoW API / Variables
+local GetBindingAction = GetBindingAction
+local SetBinding = SetBinding
+
+function RI:MailCommander_CheckBinding()
+    if GetBindingAction('SHIFT-P') ~= 'TOGGLECOLLECTIONS' then
+        -- MailCommander doing bad stuff
+        SetBinding('SHIFT-P', 'TOGGLECOLLECTIONS')
+    end
+end
+
+function RI:MailCommander()
+    self:SecureHook(_G.MailCommander, 'OnInitialized', 'MailCommander_CheckBinding')
+    self:MailCommander_CheckBinding()
+end
+
+RI:RegisterInjection(RI.MailCommander, 'MailCommander')
