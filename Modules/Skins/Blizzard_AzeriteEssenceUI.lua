f0d7a49bac75bf30dfad867219d70d1f237f90b3
LiangYuxuan <lyx@lyx.es>
2020-03-14 11:43:01 +0800

    skins: fix essence skin miss coding

--- Modules/Skins/Blizzard_AzeriteEssenceUI.lua
+++ Modules/Skins/Blizzard_AzeriteEssenceUI.lua
@@ -20,7 +20,7 @@ function RS:Blizzard_AzeriteEssenceUI()
                 if button == 'MiddleButton' then
                     self._rhythmNext = (self._rhythmNext or 0) + 1
                     GameTooltip:SetOwner(self, 'ANCHOR_RIGHT')
-                    GameTooltip:SetAzeriteEssence(self.essenceID, (self.rank + self._abyuiNext) % 4 )
+                    GameTooltip:SetAzeriteEssence(self.essenceID, (self.rank + self._rhythmNext) % 4 )
                     GameTooltip:AddLine("中键点击查看下一级", 0, 1, 0)
                     GameTooltip:Show()
                 end
