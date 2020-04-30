3ec8ab631ada3d6a8a20b5f85ec4b0c8a85d7fa7
LiangYuxuan <lyx@lyx.es>
2020-04-30 14:47:18 +0800

    skins: move CharacterFrame's RankFrame to 'TOP'

---
+++ Modules/Skins/Character.lua
@@ -0,0 +1,25 @@
+local R, E, L, V, P, G = unpack(select(2, ...))
+
+if R.Classic then return end
+
+local RS = R:GetModule('Skins')
+
+-- Lua functions
+
+-- WoW API / Variables
+
+local function AdjustRankFrame(self)
+    self.RankFrame:ClearAllPoints()
+    self.RankFrame:SetPoint('TOP', self, 'TOP', 0, 2)
+    self.RankFrame.Label:SetFont(E.Libs.LSM:Fetch('font', 'Naowh'), 14, 'OUTLINE')
+end
+
+function RS:CharacterFrame()
+	for _, Slot in pairs({_G.PaperDollItemsFrame:GetChildren()}) do
+		if Slot:IsObjectType('Button') or Slot:IsObjectType('ItemButton') then
+            hooksecurefunc(Slot, 'DisplayAsAzeriteItem', AdjustRankFrame)
+        end
+    end
+end
+
+RS:RegisterSkin(RS.CharacterFrame)
