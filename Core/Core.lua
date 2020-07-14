297d836c0ca6efd9c0b55edd7c909783ce35baf8
LiangYuxuan <lyx@lyx.es>
2020-07-14 17:14:38 +0800

    core: remove private setting apply, can change private profile in config after ElvUI 11.42

--- Core/Core.lua
+++ Core/Core.lua
@@ -9,61 +9,9 @@ local ipairs, loadstring, pairs, setfenv, tinsert, xpcall = ipairs, loadstring, 
 
 BINDING_HEADER_RHYTHM = "|cFF70B8FFRhythm Box|r"
 
-local reloadSetting = {
-    ['E.private.skins.parchmentRemoverEnable'] = 'true',
-    ['E.private.general.chatBubbles'] = '"disabled"',
-}
-
-local restartSetting = {
-    ['E.private.general.dmgfont'] = '"GothamNarrowUltra"',
-    ['E.private.general.namefont'] = '"Naowh"',
-}
-
-function R:PrivateSettings()
-    local reloadRequired, restartRequired
-    local env = { E = E }
-
-    for index, setting in ipairs({reloadSetting, restartSetting}) do
-        for var, value in pairs(setting) do
-            local func, err = loadstring('return ' .. var .. ' == ' .. value)
-            if err then
-                R.ErrorHandler(err)
-            else
-                setfenv(func, env)
-                local status, result = xpcall(func, R.ErrorHandler)
-                if status and not result then
-                    local func, err = loadstring(var .. ' = ' .. value)
-                    if err then
-                        R.ErrorHandler(err)
-                    else
-                        setfenv(func, env)
-                        local status = xpcall(func, R.ErrorHandler)
-                        if status then
-                            R:Print("已设定 %s 为 %s 。", var, value)
-                            if index == 1 then
-                                reloadRequired = true
-                            else
-                                restartRequired = true
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-
-    if restartRequired then
-        R:Print("以上设定需要重新启动游戏才生效。")
-    elseif reloadRequired then
-        R:Print("以上设定需要重新加载界面才成效。")
-    end
-end
-
 function R:Initialize()
     tinsert(E.ConfigModeLayouts, #(E.ConfigModeLayouts) + 1, 'RHYTHMBOX')
     E.ConfigModeLocalizedStrings['RHYTHMBOX'] = "|cFF70B8FFRhythm Box|r"
-
-    self:PrivateSettings()
 
     self:ToolboxInitialize()
 end
