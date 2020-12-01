0f0db5f79058b72f28766b10e42530e04654bbca
LiangYuxuan <lyx@lyx.es>
2020-12-01 16:43:38 +0800

    skin: update tdBattlePetScript

--- Modules/Skins/tdBattlePetScript.lua
+++ Modules/Skins/tdBattlePetScript.lua
@@ -9,26 +9,24 @@ local RS = R:GetModule('Skins')
 local _G = _G
 
 -- WoW API / Variables
-local CreateFrame = CreateFrame
 local hooksecurefunc = hooksecurefunc
 
 function RS:tdBattlePetScript()
-    local barWidth = _G.ElvUIPetBattleActionBar:GetWidth()
-    local anchor = CreateFrame('Frame', nil, _G.ElvUIPetBattleActionBar)
-    anchor:SetSize(barWidth / 2, 1)
-    anchor:ClearAllPoints()
-    anchor:SetPoint('BOTTOMRIGHT', _G.ElvUIPetBattleActionBar, 'TOP', 0, E.PixelMode and -2 or 0)
+    local bar = _G.ElvUIPetBattleActionBar
+    local barWidth = bar:GetWidth()
 
     local SkipButton = _G.PetBattleFrame.BottomFrame.TurnTimer.SkipButton
-    SkipButton:SetParent(anchor)
     SkipButton:SetWidth(barWidth / 2 + (E.PixelMode and 0.5 or 0))
-    SkipButton:SetPoint('BOTTOM', anchor, 'TOP', 0, E.PixelMode and -1 or 1)
-    hooksecurefunc(SkipButton, 'SetPoint', function(_, point, _, anchorPoint, xOffset, yOffset)
-        if point ~= 'BOTTOM' or anchorPoint ~= 'TOP' or xOffset ~= 0 or yOffset ~= (E.PixelMode and -1 or 1) then
+    SkipButton:SetPoint('BOTTOMRIGHT', bar, 'TOP', 0, E.PixelMode and -1 or 1, true)
+    hooksecurefunc(SkipButton, 'SetPoint', function(_, point, _, _, _, _, forced)
+        if point ~= 'BOTTOMRIGHT' and forced then
             SkipButton:ClearAllPoints()
-            SkipButton:SetPoint('BOTTOM', anchor, 'TOP', 0, E.PixelMode and -1 or 1)
+            SkipButton:SetPoint('BOTTOMRIGHT', bar, 'TOP', 0, E.PixelMode and -1 or 1, true)
         end
     end)
+
+    local xpBar = _G.PetBattleFrame.BottomFrame.xpBar
+    xpBar:SetPoint('BOTTOM', SkipButton, 'TOPRIGHT', 0, E.PixelMode and 0 or 3)
 
     local AutoButton = _G.tdBattlePetScriptAutoButton
     AutoButton:SetWidth(barWidth / 2 + (E.PixelMode and 0.5 or 0))
