9984fa4b3dc1bce071e405efd878d316e7c2552c
LiangYuxuan <lyx@lyx.es>
2020-12-22 11:01:08 +0800

    misc: introducing PrintFinder placeholder code to find out which addon is printing

---
+++ Modules/Misc/PrintFinder.lua
@@ -0,0 +1,23 @@
+local R, E, L, V, P, G = unpack(select(2, ...))
+local PF = R:NewModule('PrintFinder', 'AceEvent-3.0')
+
+-- Lua functions
+local format = format
+
+-- WoW API / Variables
+local debugstack = debugstack
+local hooksecurefunc = hooksecurefunc
+
+PF.targets = {
+    ['CONTENT'] = true,
+}
+
+function PF:Initialize()
+    hooksecurefunc('print', function(content)
+        if PF.targets[content] then
+            R:Debug(format("Caller: %s, Debug Stack: %s", debugstack(2, 1, 0), debugstack(2)), 'print caller')
+        end
+    end)
+end
+
+-- R:RegisterModule(PF:GetName())
