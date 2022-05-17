5d6a7d03a81448f45d48c062590083e58a407468
LiangYuxuan <lyx@lyx.es>
2022-05-17 17:21:55 +0800

    fix: fix opening flyouts in a row (like protal and hunter pets)

--- Modules/Skins/PortalName.lua
+++ Modules/Skins/PortalName.lua
@@ -23,37 +23,40 @@ for _ in pairs(spellIDToName) do
     length = length + 1
 end
 
-local function OnShowHandler()
-    for i = 1, length do
-        local button = _G['SpellFlyoutButton' .. i]
+local function OnToggleHandler()
+    if _G.SpellFlyout:IsShown() then
+        for i = 1, length do
+            local button = _G['SpellFlyoutButton' .. i]
 
-        if button and button.spellID and spellIDToName[button.spellID] then
-            if not button.abbrText then
-                button.abbrText = button:CreateFontString(nil, 'OVERLAY')
-                button.abbrText:FontTemplate(nil, 12)
-                button.abbrText:SetTextColor(1, 1, 1, 1)
-                button.abbrText:SetPoint('CENTER', button, 'CENTER', 0, 0)
-                button.abbrText:SetJustifyH('CENTER')
+            if button then
+                if button.spellID and spellIDToName[button.spellID] then
+                    if not button.abbrText then
+                        button.abbrText = button:CreateFontString(nil, 'OVERLAY')
+                        button.abbrText:FontTemplate(nil, 12)
+                        button.abbrText:SetTextColor(1, 1, 1, 1)
+                        button.abbrText:SetPoint('CENTER', button, 'CENTER', 0, 0)
+                        button.abbrText:SetJustifyH('CENTER')
+                    end
+
+                    button.abbrText:SetText(spellIDToName[button.spellID])
+                elseif button.abbrText then
+                    button.abbrText:SetText('')
+                end
             end
-
-            button.abbrText:SetText(spellIDToName[button.spellID])
         end
-    end
-end
+    else
+        for i = 1, length do
+            local button = _G['SpellFlyoutButton' .. i]
 
-local function OnHideHandler()
-    for i = 1, length do
-        local button = _G['SpellFlyoutButton' .. i]
-
-        if button and button.abbrText then
-            button.abbrText:SetText('')
+            if button and button.abbrText then
+                button.abbrText:SetText('')
+            end
         end
     end
 end
 
 function RS:PortalName()
-    _G.SpellFlyout:HookScript('OnShow', OnShowHandler)
-    _G.SpellFlyout:HookScript('OnHide', OnHideHandler)
+    self:SecureHook(_G.SpellFlyout, 'Toggle', OnToggleHandler)
 end
 
 RS:RegisterSkin(RS.PortalName)
