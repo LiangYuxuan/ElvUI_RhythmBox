7ef1f6f9825925ed79626f3e7689bd145c6a1427
LiangYuxuan <lyx@lyx.es>
2022-08-17 11:33:54 +0800

    feat(Injections): open DoYouNeedThat window after M+ completed

---
+++ Modules/Injections/DoYouNeedThat.lua
@@ -0,0 +1,19 @@
+local R, E, L, V, P, G = unpack(select(2, ...))
+local RI = R:GetModule('Injections')
+
+-- Lua functions
+local _G = _G
+
+-- WoW API / Variables
+
+function RI:DoYouNeedThat()
+    local DoYouNeedThat = _G.DoYouNeedThat
+
+    DoYouNeedThat.EventFrame:RegisterEvent('CHALLENGE_MODE_COMPLETED')
+    DoYouNeedThat.CHALLENGE_MODE_COMPLETED = function(self)
+        self:ClearEntries()
+        self.lootFrame:Show()
+    end
+end
+
+RI:RegisterInjection(RI.DoYouNeedThat, 'DoYouNeedThat')
