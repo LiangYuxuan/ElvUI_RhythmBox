1e4988f5b52c7f3f7717b827fa910ed631225f0d
LiangYuxuan <lyx@lyx.es>
2022-10-31 11:15:35 +0800

    feat(Skins): tweak ElvUI UF frame strata

---
+++ Modules/Skins/ElvUI_UnitFrames.lua
@@ -0,0 +1,11 @@
+local R, E, L, V, P, G = unpack(select(2, ...))
+local RS = R:GetModule('Skins')
+
+function RS:ElvUIUnitFrames()
+    C_Timer.After(5, function()
+        _G.ElvUF_Player.RaisedElementParent:SetFrameStrata("MEDIUM")
+        _G.ElvUF_TargetBuffs:SetFrameStrata("BACKGROUND")
+    end)
+end
+
+RS:RegisterSkin(RS.ElvUIUnitFrames)
