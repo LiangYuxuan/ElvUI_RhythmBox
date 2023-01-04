54690d8018aa48491146c6ef161468b1eecc2a4b
LiangYuxuan <lyx@lyx.es>
2023-01-04 11:08:33 +0800

    fix(Skins): fix tdBattlePetScript for new ElvUI skin

--- Modules/Skins/tdBattlePetScript.lua
+++ Modules/Skins/tdBattlePetScript.lua
@@ -15,8 +15,8 @@ function RS:tdBattlePetScript()
     local SkipButton = _G.PetBattleFrame.BottomFrame.TurnTimer.SkipButton
     SkipButton:SetWidth(barWidth / 2 + (E.PixelMode and 0.5 or 0))
     SkipButton:SetPoint('BOTTOMRIGHT', bar, 'TOP', 0, E.PixelMode and -1 or 1, true)
-    hooksecurefunc(SkipButton, 'SetPoint', function(_, point, _, _, _, _, forced)
-        if point ~= 'BOTTOMRIGHT' and forced then
+    hooksecurefunc(SkipButton, 'SetPoint', function(_, point, _, relativePoint, _, _, forced)
+        if (point ~= 'BOTTOMRIGHT' or relativePoint ~= 'TOP') and forced then
             SkipButton:ClearAllPoints()
             SkipButton:SetPoint('BOTTOMRIGHT', bar, 'TOP', 0, E.PixelMode and -1 or 1, true)
         end
