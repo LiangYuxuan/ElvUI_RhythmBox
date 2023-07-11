0aa67c2bfb7c9419bb87370277006ec4ce4d15e3
LiangYuxuan <lyx@lyx.es>
2023-07-11 10:39:55 +0800

    fix(Skins): fix paragon text & reputation bar

--- Modules/Skins/Reputation.lua
+++ Modules/Skins/Reputation.lua
@@ -10,28 +10,24 @@ local BreakUpLargeNumbers = BreakUpLargeNumbers
 local C_Reputation_GetFactionParagonInfo = C_Reputation.GetFactionParagonInfo
 local C_Reputation_IsFactionParagon = C_Reputation.IsFactionParagon
 local GetFactionInfo = GetFactionInfo
-local GetNumFactions = GetNumFactions
 local hooksecurefunc = hooksecurefunc
-
-local FauxScrollFrame_GetOffset = FauxScrollFrame_GetOffset
 
 local ARCHAEOLOGY_COMPLETION = ARCHAEOLOGY_COMPLETION
 local FACTION_BAR_COLORS = FACTION_BAR_COLORS
 local FONT_COLOR_CODE_CLOSE = FONT_COLOR_CODE_CLOSE
 local HIGHLIGHT_FONT_COLOR_CODE = HIGHLIGHT_FONT_COLOR_CODE
-local NUM_FACTIONS_DISPLAYED = NUM_FACTIONS_DISPLAYED
 local REPUTATION_PROGRESS_FORMAT = REPUTATION_PROGRESS_FORMAT
 local TOOLTIP_QUEST_REWARDS_STYLE_DEFAULT = TOOLTIP_QUEST_REWARDS_STYLE_DEFAULT
 
 function RS:ReputationFrame()
-    hooksecurefunc('EmbeddedItemTooltip_SetItemByQuestReward', function(tooltip)
-        if tooltip == _G.GameTooltip.ItemTooltip and _G.GameTooltip.factionID then
-            local frame = _G[_G.GameTooltip:GetName() .. 'TextLeft' .. _G.GameTooltip:NumLines()]
-            if frame:GetText() == TOOLTIP_QUEST_REWARDS_STYLE_DEFAULT.headerText then
+    hooksecurefunc('ReputationParagonFrame_SetupParagonTooltip', function()
+        if _G.GameTooltip.factionID then
+            local text = _G[_G.GameTooltip:GetName() .. 'TextLeft' .. _G.GameTooltip:NumLines()]
+            if text:GetText() == TOOLTIP_QUEST_REWARDS_STYLE_DEFAULT.headerText then
                 local currentValue, threshold, _, hasRewardPending = C_Reputation_GetFactionParagonInfo(_G.GameTooltip.factionID)
                 if currentValue then
                     local completionCount = floor(currentValue / threshold) - (hasRewardPending and 1 or 0)
-                    frame:SetText(frame:GetText() .. "  ( " ..
+                    text:SetText(text:GetText() .. "  ( " ..
                         format(ARCHAEOLOGY_COMPLETION, completionCount) .. " )"
                     )
                 end
@@ -39,35 +35,26 @@ function RS:ReputationFrame()
         end
     end)
 
-    hooksecurefunc('ReputationFrame_Update', function()
-        local numFactions = GetNumFactions()
-        local factionOffset = FauxScrollFrame_GetOffset(_G.ReputationListScrollFrame)
-        for i = 1, NUM_FACTIONS_DISPLAYED, 1 do
-            local factionIndex = factionOffset + i
-            if (factionIndex > numFactions) then break end
+    hooksecurefunc('ReputationFrame_InitReputationRow', function(factionRow, elementData)
+        local factionIndex = elementData.index
+        local factionContainer = factionRow.Container
+        local factionBar = factionContainer.ReputationBar
+        local factionStanding = factionBar.FactionStanding
 
-            local factionID = select(14, GetFactionInfo(factionIndex))
-            if (factionID and C_Reputation_IsFactionParagon(factionID)) then
-                local currentValue, threshold, _, hasRewardPending = C_Reputation_GetFactionParagonInfo(factionID)
-                local barMax, barValue = threshold, (currentValue % threshold) + (hasRewardPending and threshold or 0)
-                local colorIndex = min(floor((barValue / barMax) * 10) + 1, 8)
-                local color = FACTION_BAR_COLORS[colorIndex]
-
-                local factionRow = _G['ReputationBar' .. i]
-                local factionBar = _G['ReputationBar' .. i .. 'ReputationBar']
-                local factionStanding = _G['ReputationBar' .. i .. 'ReputationBarFactionStanding']
-
-                factionRow.standingText = factionRow.standingText .. "+"
-                factionRow.rolloverText = HIGHLIGHT_FONT_COLOR_CODE .. " " ..
-                    format(REPUTATION_PROGRESS_FORMAT, BreakUpLargeNumbers(barValue), BreakUpLargeNumbers(barMax)) ..
-                    FONT_COLOR_CODE_CLOSE
-
-                factionBar:SetMinMaxValues(0, barMax)
-                factionBar:SetValue(barValue)
-                factionBar:SetStatusBarColor(color.r, color.g, color.b)
-
-                factionStanding:SetText(factionRow.standingText)
-            end
+        local factionID = select(14, GetFactionInfo(factionIndex))
+        if (factionID and C_Reputation_IsFactionParagon(factionID)) then
+            local currentValue, threshold, _, hasRewardPending = C_Reputation_GetFactionParagonInfo(factionID)
+            local barMax, barValue = threshold, (currentValue % threshold) + (hasRewardPending and threshold or 0)
+            local colorIndex = min(floor((barValue / barMax) * 10) + 1, 8)
+            local color = FACTION_BAR_COLORS[colorIndex]
+            factionRow.standingText = factionRow.standingText .. "+"
+            factionRow.rolloverText = HIGHLIGHT_FONT_COLOR_CODE .. " " ..
+                format(REPUTATION_PROGRESS_FORMAT, BreakUpLargeNumbers(barValue), BreakUpLargeNumbers(barMax)) ..
+                FONT_COLOR_CODE_CLOSE
+            factionBar:SetMinMaxValues(0, barMax)
+            factionBar:SetValue(barValue)
+            factionBar:SetStatusBarColor(color.r, color.g, color.b)
+            factionStanding:SetText(factionRow.standingText)
         end
     end)
 end
