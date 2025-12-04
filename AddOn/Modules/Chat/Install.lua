310ab5e4db215b38a2e4d8d82770db8e68a2ef72
LiangYuxuan <lyx@lyx.es>
2025-12-04 12:09:11 +0800

    refactor: use new api in patch 11.2.7

--- AddOn/Modules/Chat/Install.lua
+++ AddOn/Modules/Chat/Install.lua
@@ -10,19 +10,15 @@ local ipairs, tinsert = ipairs, tinsert
 local ChangeChatColor = ChangeChatColor
 local JoinPermanentChannel = JoinPermanentChannel
 
-local ChatFrame_AddChannel = ChatFrame_AddChannel
-local ChatFrame_AddMessageGroup = ChatFrame_AddMessageGroup
-local ChatFrame_RemoveAllMessageGroups = ChatFrame_RemoveAllMessageGroups
 local CombatConfig_SetCombatFiltersToDefault = CombatConfig_SetCombatFiltersToDefault
 local FCF_DockFrame = FCF_DockFrame
 local FCF_OpenNewWindow = FCF_OpenNewWindow
+local FCF_ResetChatWindow = FCF_ResetChatWindow
 local FCF_ResetChatWindows = FCF_ResetChatWindows
 local FCF_SavePositionAndDimensions = FCF_SavePositionAndDimensions
 local FCF_SetChatWindowFontSize = FCF_SetChatWindowFontSize
-local FCF_SetLocked = FCF_SetLocked
 local FCF_SetWindowName = FCF_SetWindowName
 local FCF_StopDragging = FCF_StopDragging
-local FCF_UnDockFrame = FCF_UnDockFrame
 local ToggleChatColorNamesByClassGroup = ToggleChatColorNamesByClassGroup
 local VoiceTranscriptionFrame_UpdateEditBox = VoiceTranscriptionFrame_UpdateEditBox
 local VoiceTranscriptionFrame_UpdateVisibility = VoiceTranscriptionFrame_UpdateVisibility
@@ -32,103 +28,90 @@ local GENERAL = GENERAL
 local GROUPS = GROUPS
 local GUILD = GUILD
 local GUILD_EVENT_LOG = GUILD_EVENT_LOG
-local NUM_CHAT_WINDOWS = NUM_CHAT_WINDOWS
+local VOICE = VOICE
 
 function C:InstallChat()
     CombatConfig_SetCombatFiltersToDefault()
-    for i = 1, 2 do
+    for i = 1, #_G.Blizzard_CombatLog_Filters.filters do
         _G.Blizzard_CombatLog_Filters.filters[i].settings.timestamp = true
         _G.Blizzard_CombatLog_Filters.filters[i].settings.braces = true
         _G.Blizzard_CombatLog_Filters.filters[i].settings.spellBraces = true
     end
 
+    local chatFrames = _G.CHAT_FRAMES
     FCF_ResetChatWindows()
 
-    FCF_SetLocked(_G.ChatFrame1, 1)
-    FCF_DockFrame(_G.ChatFrame2)
-    FCF_SetLocked(_G.ChatFrame2, 1)
-    FCF_DockFrame(_G.ChatFrame3)
-    FCF_SetLocked(_G.ChatFrame3, 1)
+    -- force initialize the tts chat (it doesn't get shown unless you use it)
+    local voiceChat = _G[chatFrames[3]]
+    FCF_ResetChatWindow(voiceChat, VOICE)
+    FCF_DockFrame(voiceChat, 3)
 
-    FCF_OpenNewWindow(GUILD)
-    FCF_UnDockFrame(_G.ChatFrame4)
-    FCF_SetLocked(_G.ChatFrame4, 1)
-    _G.ChatFrame4:Show()
+    local guildChat = FCF_OpenNewWindow(GUILD)
+    FCF_DockFrame(guildChat)
 
-    FCF_OpenNewWindow(GROUPS)
-    FCF_UnDockFrame(_G.ChatFrame5)
-    FCF_SetLocked(_G.ChatFrame5, 1)
-    _G.ChatFrame5:Show()
+    local groupChat = FCF_OpenNewWindow(GROUPS)
+    FCF_DockFrame(groupChat)
 
-    FCF_OpenNewWindow("PM")
-    FCF_UnDockFrame(_G.ChatFrame6)
-    FCF_SetLocked(_G.ChatFrame6, 1)
-    _G.ChatFrame6:Show()
+    local pmChat = FCF_OpenNewWindow("PM")
+    FCF_DockFrame(pmChat)
 
-    for i = 1, NUM_CHAT_WINDOWS do
-        local frame = _G['ChatFrame' .. i]
+    for id, frameName in ipairs(chatFrames) do
+        local frame = _G[frameName]
 
-        CH:FCFTab_UpdateColors(CH:GetTab(frame))
+        if E.private.chat.enable then
+            CH:FCFTab_UpdateColors(CH:GetTab(frame))
+        end
 
-        if i == 4 or i == 5 or i == 6 then
-            FCF_UnDockFrame(frame)
+        if id == 1 then
             frame:ClearAllPoints()
-            frame:SetPoint('BOTTOMLEFT', _G.LeftChatToggleButton, 'TOPLEFT', 1, 3)
-            FCF_DockFrame(frame)
-            FCF_SetLocked(frame, 1)
-            frame:Show()
-        elseif i == 3 then
+            frame:Point('BOTTOMLEFT', _G.LeftChatToggleButton, 'TOPLEFT', 1, 3)
+            FCF_SetWindowName(frame, GENERAL)
+        elseif id == 2 then
+            FCF_SetWindowName(frame, GUILD_EVENT_LOG)
+        elseif id == 3 then
             VoiceTranscriptionFrame_UpdateVisibility(frame)
             VoiceTranscriptionFrame_UpdateVoiceTab(frame)
             VoiceTranscriptionFrame_UpdateEditBox(frame)
-        end
-
-        FCF_SavePositionAndDimensions(frame)
-        FCF_StopDragging(frame)
-
-        -- set default Elvui font size
-        FCF_SetChatWindowFontSize(nil, frame, 12)
-
-        if i == 1 then
-            FCF_SetWindowName(frame, GENERAL)
-        elseif i == 2 then
-            FCF_SetWindowName(frame, GUILD_EVENT_LOG)
-        elseif i == 4 then
+        elseif id == 4 then
             FCF_SetWindowName(frame, GUILD)
-        elseif i == 5 then
+        elseif id == 5 then
             FCF_SetWindowName(frame, GROUPS)
-        elseif i == 6 then
+        elseif id == 6 then
             FCF_SetWindowName(frame, "PM")
         end
+
+        FCF_SetChatWindowFontSize(nil, frame, 12)
+        FCF_SavePositionAndDimensions(frame)
+        FCF_StopDragging(frame)
     end
 
     local chatGroup = { 'SYSTEM', 'CHANNEL', 'SAY', 'EMOTE', 'YELL', 'WHISPER', 'PARTY', 'PARTY_LEADER', 'RAID', 'RAID_LEADER', 'RAID_WARNING', 'INSTANCE_CHAT', 'INSTANCE_CHAT_LEADER', 'GUILD', 'OFFICER', 'MONSTER_SAY', 'MONSTER_YELL', 'MONSTER_EMOTE', 'MONSTER_WHISPER', 'MONSTER_BOSS_EMOTE', 'MONSTER_BOSS_WHISPER', 'ERRORS', 'AFK', 'DND', 'IGNORED', 'BG_HORDE', 'BG_ALLIANCE', 'BG_NEUTRAL', 'ACHIEVEMENT', 'GUILD_ACHIEVEMENT', 'BN_WHISPER', 'BN_INLINE_TOAST_ALERT', 'COMBAT_XP_GAIN', 'COMBAT_HONOR_GAIN', 'COMBAT_FACTION_CHANGE', 'SKILL', 'LOOT', 'CURRENCY', 'MONEY', 'PING' }
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame1)
+    _G.ChatFrame1:RemoveAllMessageGroups()
     for _, v in ipairs(chatGroup) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame1, v)
+        _G.ChatFrame1:AddMessageGroup(v)
     end
 
     chatGroup = { 'GUILD', 'OFFICER', 'GUILD_ACHIEVEMENT', 'GUILD_ITEM_LOOTED' }
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame4)
+    guildChat:RemoveAllMessageGroups()
     for _, v in ipairs(chatGroup) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame4, v)
+        guildChat:AddMessageGroup(v)
     end
 
     chatGroup = { 'PARTY', 'PARTY_LEADER', 'RAID', 'RAID_LEADER', 'RAID_WARNING', 'INSTANCE_CHAT', 'INSTANCE_CHAT_LEADER' }
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame5)
+    groupChat:RemoveAllMessageGroups()
     for _, v in ipairs(chatGroup) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame5, v)
+        groupChat:AddMessageGroup(v)
     end
 
     chatGroup = { 'WHISPER', 'BN_WHISPER' }
-    ChatFrame_RemoveAllMessageGroups(_G.ChatFrame6)
+    pmChat:RemoveAllMessageGroups()
     for _, v in ipairs(chatGroup) do
-        ChatFrame_AddMessageGroup(_G.ChatFrame6, v)
+        pmChat:AddMessageGroup(v)
     end
 
-    ChatFrame_AddChannel(_G.ChatFrame1, GENERAL)
+    _G.ChatFrame1:AddChannel(GENERAL)
     JoinPermanentChannel('大脚世界频道', nil, 1)
-    ChatFrame_AddChannel(_G.ChatFrame1, '大脚世界频道')
+    _G.ChatFrame1:AddChannel('大脚世界频道')
 
     -- set the chat groups names in class color to enabled for all chat groups which players names appear
     chatGroup = { 'SAY', 'EMOTE', 'YELL', 'WHISPER', 'PARTY', 'PARTY_LEADER', 'RAID', 'RAID_LEADER', 'RAID_WARNING', 'INSTANCE_CHAT', 'INSTANCE_CHAT_LEADER', 'GUILD', 'OFFICER', 'ACHIEVEMENT', 'GUILD_ACHIEVEMENT', 'COMMUNITIES_CHANNEL' }
@@ -140,12 +123,12 @@ function C:InstallChat()
     end
 
     -- Adjust Chat Colors
-    ChangeChatColor('CHANNEL1', 195/255, 230/255, 232/255) -- General
-    ChangeChatColor('CHANNEL2', 232/255, 158/255, 121/255) -- Trade
-    ChangeChatColor('CHANNEL3', 232/255, 228/255, 121/255) -- Local Defense
+    ChangeChatColor('CHANNEL1', 0.76, 0.90, 0.91) -- General
+    ChangeChatColor('CHANNEL2', 0.91, 0.62, 0.47) -- Trade
+    ChangeChatColor('CHANNEL3', 0.91, 0.89, 0.47) -- Local Defense
 
     if E.private.chat.enable then
-        E:GetModule('Chat'):PositionChats()
+        CH:PositionChats()
     end
 
     if E.db.RightChatPanelFaded then
