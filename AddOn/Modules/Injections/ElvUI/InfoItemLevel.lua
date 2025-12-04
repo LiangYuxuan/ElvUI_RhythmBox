15cb64730c093b59cf2e6b58c73c536016f4ccd6
LiangYuxuan <3353011+LiangYuxuan@users.noreply.github.com>
2025-12-04 01:47:14 +0000

    chore: regenerate

--- AddOn/Modules/Injections/ElvUI/InfoItemLevel.lua
+++ AddOn/Modules/Injections/ElvUI/InfoItemLevel.lua
@@ -186,67 +186,67 @@ local tierSetItemIDs = {
     -- Warrior
     -- Chains of the Living Weapon
     [237613] = 1, [237611] = 1, [237610] = 1, [237609] = 1, [237608] = 1,
-    -- 12.0 Class Set - Warrior
+    -- Rage of the Night Ender
     [249955] = 2, [249953] = 2, [249952] = 2, [249951] = 2, [249950] = 2,
     -- Paladin
     -- Vows of the Lucent Battalion
     [237622] = 1, [237620] = 1, [237619] = 1, [237618] = 1, [237617] = 1,
-    -- 12.0 Class Set - Paladin
+    -- Luminant Verdict's Vestments
     [249964] = 2, [249962] = 2, [249961] = 2, [249960] = 2, [249959] = 2,
     -- Hunter
     -- Midnight Herald's Pledge
     [237649] = 1, [237647] = 1, [237646] = 1, [237645] = 1, [237644] = 1,
-    -- 12.0 Class Set - Hunter
+    -- Primal Sentry's Camouflage
     [249991] = 2, [249989] = 2, [249988] = 2, [249987] = 2, [249986] = 2,
     -- Rogue
     -- Shroud of the Sudden Eclipse
     [237667] = 1, [237665] = 1, [237664] = 1, [237663] = 1, [237662] = 1,
-    -- 12.0 Class Set - Rogue
+    -- Motley of the Grim Jest
     [250009] = 2, [250007] = 2, [250006] = 2, [250005] = 2, [250004] = 2,
     -- Priest
     -- Eulogy to a Dying Star
     [237710] = 1, [237709] = 1, [237708] = 1, [237712] = 1, [237707] = 1,
-    -- 12.0 Class Set - Priest
+    -- Blind Oath's Burden
     [250052] = 2, [250051] = 2, [250050] = 2, [250054] = 2, [250049] = 2,
     -- Death Knight
     -- Hollow Sentinel's Wake
     [237631] = 1, [237629] = 1, [237628] = 1, [237627] = 1, [237626] = 1,
-    -- 12.0 Class Set - Death Knight
+    -- Relentless Rider's Lament
     [249973] = 2, [249971] = 2, [249970] = 2, [249969] = 2, [249968] = 2,
     -- Shaman
     -- Howls of Channeled Fury
     [237640] = 1, [237638] = 1, [237637] = 1, [237636] = 1, [237635] = 1,
-    -- 12.0 Class Set - Shaman
+    -- Mantle of the Primal Core
     [249982] = 2, [249980] = 2, [249979] = 2, [249978] = 2, [249977] = 2,
     -- Mage
     -- Augur's Ephemeral Plumage
     [237721] = 1, [237719] = 1, [237718] = 1, [237717] = 1, [237716] = 1,
-    -- 12.0 Class Set - Mage
+    -- Voidbreaker's Accordance
     [250063] = 2, [250061] = 2, [250060] = 2, [250059] = 2, [250058] = 2,
     -- Warlock
     -- Inquisitor's Feast of Madness
     [237701] = 1, [237700] = 1, [237699] = 1, [237703] = 1, [237698] = 1,
-    -- 12.0 Class Set - Warlock
+    -- Reign of the Abyssal Immolator
     [250043] = 2, [250042] = 2, [250041] = 2, [250045] = 2, [250040] = 2,
     -- Monk
     -- Crash of Fallen Storms
     [237676] = 1, [237674] = 1, [237673] = 1, [237672] = 1, [237671] = 1,
-    -- 12.0 Class Set - Monk
+    -- Way of Ra-den's Chosen
     [250018] = 2, [250016] = 2, [250015] = 2, [250014] = 2, [250013] = 2,
     -- Druid
     -- Ornaments of the Mother Eagle
     [237685] = 1, [237683] = 1, [237682] = 1, [237681] = 1, [237680] = 1,
-    -- 12.0 Class Set - Druid
+    -- Sprouts of the Luminous Bloom
     [250027] = 2, [250025] = 2, [250024] = 2, [250023] = 2, [250022] = 2,
     -- Demon Hunter
     -- Charhound's Vicious Hunt
     [237694] = 1, [237692] = 1, [237691] = 1, [237690] = 1, [237689] = 1,
-    -- 12.0 Class Set - Demon Hunter
+    -- Devouring Reaver's Sheathe
     [250036] = 2, [250034] = 2, [250033] = 2, [250032] = 2, [250031] = 2,
     -- Evoker
     -- Spellweaver's Immaculate Design
     [237658] = 1, [237656] = 1, [237655] = 1, [237654] = 1, [237653] = 1,
-    -- 12.0 Class Set - Evoker
+    -- Livery of the Black Talon
     [250000] = 2, [249998] = 2, [249997] = 2, [249996] = 2, [249995] = 2,
     ---AUTO_GENERATED TAILING InfoItemLevelItemSets
 }
