1170b103ab0a06e8268d6605d76724e2399e415b
LiangYuxuan <lyx@lyx.es>
2026-02-25 16:29:06 +0800

    feat(RaidTools): BigWigs & DBM drops addon message feature to countdown

--- AddOn/Modules/Misc/RaidTools.lua
+++ AddOn/Modules/Misc/RaidTools.lua
@@ -4,21 +4,16 @@ local RU = E:GetModule('RaidUtility')
 
 -- Lua functions
 local _G = _G
-local print, select = print, select
 
 -- WoW API / Variables
-local C_ChatInfo_SendAddonMessage = C_ChatInfo.SendAddonMessage
 local C_ChatInfo_SendChatMessage = C_ChatInfo.SendChatMessage
 local C_PartyInfo_DoCountdown = C_PartyInfo.DoCountdown
 local GetInstanceInfo = GetInstanceInfo
 local GetNumGroupMembers = GetNumGroupMembers
 local IsInGroup = IsInGroup
 local IsInRaid = IsInRaid
-local UnitExists = UnitExists
-local UnitIsEnemy = UnitIsEnemy
 local UnitIsGroupAssistant = UnitIsGroupAssistant
 local UnitIsGroupLeader = UnitIsGroupLeader
-local UnitName = UnitName
 
 local RaidWarningFrame_OnEvent = RaidWarningFrame_OnEvent
 
@@ -35,7 +30,7 @@ function RT:SmartChat(msg)
         C_ChatInfo_SendChatMessage(msg, IsInGroup(LE_PARTY_CATEGORY_INSTANCE) and 'INSTANCE_CHAT' or 'PARTY')
     else
         RaidWarningFrame_OnEvent(_G.RaidWarningFrame, 'CHAT_MSG_RAID_WARNING', msg)
-        print(msg)
+        R:Print(msg)
     end
 end
 
@@ -51,26 +46,6 @@ function RT:RepeatChat(firstMsg)
         self:SmartChat("战斗倒计时 " .. self.restTime .. " 秒")
     end
     self.restTime = self.restTime - 1
-end
-
-function RT:SendAddOnTimers(pullTime)
-    local isInInstance = IsInGroup(LE_PARTY_CATEGORY_INSTANCE)
-    local isInParty = IsInGroup()
-    local isInRaid = IsInRaid()
-    local playerName = nil
-    local chat_type = (isInInstance and 'INSTANCE_CHAT') or (isInRaid and 'RAID') or (isInParty and 'PARTY')
-    if not chat_type then
-        chat_type = 'WHISPER'
-        playerName = UnitName('player')
-    end
-    local mapID = select(8, GetInstanceInfo())
-    local targetName = (UnitExists('target') and UnitIsEnemy('player', 'target')) and UnitName('target') or nil
-    C_ChatInfo_SendAddonMessage('BigWigs', 'P^Pull^' .. pullTime, chat_type, playerName)
-    if targetName then
-        C_ChatInfo_SendAddonMessage('D4', ('PT\t%d\t%d\t%s'):format(pullTime, mapID or -1, targetName), chat_type, playerName)
-    else
-        C_ChatInfo_SendAddonMessage('D4', ('PT\t%d\t%d'):format(pullTime, mapID or -1), chat_type, playerName)
-    end
 end
 
 function RT:GetTimeToPull()
@@ -91,7 +66,6 @@ function RT:Initialize()
                 -- Cancel Pull Timer
                 RT.restTime = nil
                 C_PartyInfo_DoCountdown(0)
-                RT:SendAddOnTimers(0)
                 if E.db.RhythmBox.Misc.PullTimerSendToChat then
                     RT:CancelTimer(RT.timer)
                     RT:SmartChat(">>> 取消 <<<")
@@ -100,7 +74,6 @@ function RT:Initialize()
                 -- Launch Pull Timer
                 RT.restTime = RT:GetTimeToPull()
                 C_PartyInfo_DoCountdown(RT.restTime)
-                RT:SendAddOnTimers(RT.restTime)
                 if E.db.RhythmBox.Misc.PullTimerSendToChat then
                     RT.timer = RT:ScheduleRepeatingTimer('RepeatChat', 1)
                     RT:RepeatChat(true)
