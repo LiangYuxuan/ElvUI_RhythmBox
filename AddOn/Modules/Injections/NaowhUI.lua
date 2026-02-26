5986608ed8dfc3d5f13d8cc55c1a6a0457f3bc24
LiangYuxuan <lyx@lyx.es>
2026-02-26 16:35:00 +0800

    feat(Injections): add ElvUI profile tweak for NaowhUI

--- AddOn/Modules/Injections/NaowhUI.lua
+++ AddOn/Modules/Injections/NaowhUI.lua
@@ -1,9 +1,12 @@
 local R, E, L, V, P, G = unpack((select(2, ...)))
 local RI = R:GetModule('Injections')
+local AB = E:GetModule('ActionBars')
+local DT = E:GetModule('DataTexts')
 
 -- Lua functions
 local _G = _G
 local ipairs, unpack = ipairs, unpack
+local hooksecurefunc = hooksecurefunc
 local string_match = string.match
 
 -- WoW API / Variables
@@ -96,6 +99,26 @@ local classSpecMap = {
     },
 }
 
+local bars = {
+    'bar1',
+    'bar2',
+    'bar3',
+    'bar4',
+    'bar5',
+    'bar6',
+}
+
+local basicBars = {
+    'bar1',
+    'bar2',
+    'bar3',
+    'bar4',
+    'bar5',
+    'bar6',
+    'barPet',
+    'stanceBar',
+}
+
 local function LoadEditModeLayout(layoutInfo)
     if not layoutInfo.layouts[layoutInfo.activeLayout] then return end
 
@@ -107,6 +130,153 @@ local function LoadEditModeLayout(layoutInfo)
         if layout.layoutName == 'Naowh' then
             C_EditMode_SetActiveLayout(Enum_EditModePresetLayoutsMeta_NumValues + index)
         end
+    end
+end
+
+local function TweakElvUIProfile()
+    for _, bar in ipairs(basicBars) do
+        local barConfig = E.db.actionbar[bar]
+
+        barConfig.backdrop = false
+        barConfig.buttonSize = 36
+        barConfig.buttonSpacing = 1
+        barConfig.enabled = true
+        barConfig.mouseover = false
+    end
+
+    E.db.actionbar.barPet.buttons = 10
+    E.db.actionbar.barPet.buttonsPerRow = 10
+    E.db.actionbar.barPet.point = 'BOTTOMLEFT'
+
+    E.db.actionbar.stanceBar.buttons = 10
+    E.db.actionbar.stanceBar.buttonsPerRow = 10
+    E.db.actionbar.stanceBar.point = 'BOTTOMLEFT'
+
+    for _, bar in ipairs(bars) do
+        local barConfig = E.db.actionbar[bar]
+
+        barConfig.buttons = 12
+        barConfig.macrotext = true
+        barConfig.macroTextPosition = 'BOTTOM'
+        barConfig.macroTextYOffset = 0
+        barConfig.targetReticle = true
+    end
+
+    E.db.actionbar.bar1.buttonsPerRow = 12
+    E.db.actionbar.bar1.point = 'BOTTOMLEFT'
+    E.db.actionbar.bar1.showGrid = true
+
+    E.db.actionbar.bar2.buttonsPerRow = 12
+    E.db.actionbar.bar2.point = 'BOTTOMLEFT'
+    E.db.actionbar.bar2.showGrid = false
+
+    E.db.actionbar.bar3.buttonsPerRow = 6
+    E.db.actionbar.bar3.point = 'BOTTOMLEFT'
+    E.db.actionbar.bar3.showGrid = true
+
+    E.db.actionbar.bar4.buttonsPerRow = 1
+    E.db.actionbar.bar4.point = 'TOPLEFT'
+    E.db.actionbar.bar4.showGrid = false
+
+    E.db.actionbar.bar5.buttonsPerRow = 12
+    E.db.actionbar.bar5.point = 'BOTTOMLEFT'
+    E.db.actionbar.bar5.showGrid = true
+
+    E.db.actionbar.bar6.buttonsPerRow = 6
+    E.db.actionbar.bar6.point = 'BOTTOMLEFT'
+    E.db.actionbar.bar6.showGrid = true
+
+    AB:UpdateButtonSettings()
+
+    _G.ElvAB_1:ClearAllPoints()
+    _G.ElvAB_1:SetPoint('BOTTOM', E.UIParent, 'BOTTOM', 0, 1)
+    E:SaveMoverPosition(_G.ElvAB_1.name)
+
+    _G.ElvAB_2:ClearAllPoints()
+    _G.ElvAB_2:SetPoint('BOTTOM', E.UIParent, 'BOTTOM', 0, 75)
+    E:SaveMoverPosition(_G.ElvAB_2.name)
+
+    _G.ElvAB_3:ClearAllPoints()
+    _G.ElvAB_3:SetPoint('BOTTOM', E.UIParent, 'BOTTOM', -335, 1)
+    E:SaveMoverPosition(_G.ElvAB_3.name)
+
+    _G.ElvAB_4:ClearAllPoints()
+    _G.ElvAB_4:SetPoint('TOPRIGHT', E.UIParent, 'TOPRIGHT', 0, -500)
+    E:SaveMoverPosition(_G.ElvAB_4.name)
+
+    _G.ElvAB_5:ClearAllPoints()
+    _G.ElvAB_5:SetPoint('BOTTOM', E.UIParent, 'BOTTOM', 0, 38)
+    E:SaveMoverPosition(_G.ElvAB_5.name)
+
+    _G.ElvAB_6:ClearAllPoints()
+    _G.ElvAB_6:SetPoint('BOTTOM', E.UIParent, 'BOTTOM', 335, 1)
+    E:SaveMoverPosition(_G.ElvAB_6.name)
+
+    _G.PetAB:ClearAllPoints()
+    _G.PetAB:SetPoint('BOTTOM', E.UIParent, 'BOTTOM', 0, 112)
+    E:SaveMoverPosition(_G.PetAB.name)
+
+    _G.ShiftAB:ClearAllPoints()
+    _G.ShiftAB:SetPoint('BOTTOM', E.UIParent, 'BOTTOM', -335, 75)
+    E:SaveMoverPosition(_G.ShiftAB.name)
+
+    _G.ABSlotItemAnchorMover:ClearAllPoints()
+    _G.ABSlotItemAnchorMover:SetPoint('BOTTOMRIGHT', E.UIParent, 'BOTTOMRIGHT', -560, 242)
+    E:SaveMoverPosition(_G.ABSlotItemAnchorMover.name)
+
+    _G.ABQuestItemAnchorMover:ClearAllPoints()
+    _G.ABQuestItemAnchorMover:SetPoint('BOTTOMRIGHT', E.UIParent, 'BOTTOMRIGHT', -560, 200)
+    E:SaveMoverPosition(_G.ABQuestItemAnchorMover.name)
+
+    _G.RhythmBoxQuickMacroContainerMover:ClearAllPoints()
+    _G.RhythmBoxQuickMacroContainerMover:SetPoint('BOTTOMRIGHT', E.UIParent, 'BOTTOMRIGHT', -560, 158)
+    E:SaveMoverPosition(_G.RhythmBoxQuickMacroContainerMover.name)
+
+    local panelName = 'TopPanel'
+    if not E.db.datatexts.panels[panelName] then
+        E.db.datatexts.panels[panelName] = { enable = true, battleground = false }
+    end
+
+    local panelProfile = E.db.datatexts.panels[panelName]
+    panelProfile.enable = true
+    panelProfile[1] = 'LDB_EventTimetable'
+    panelProfile[2] = 'Combat'
+    panelProfile[3] = 'Target Range'
+    panelProfile[4] = 'Coords'
+    panelProfile[5] = 'Time'
+
+    if not E.global.datatexts.customPanels[panelName] then
+        E.global.datatexts.customPanels[panelName] = E:CopyTable({}, G.datatexts.newPanelInfo)
+
+        local panelGlobal = E.global.datatexts.customPanels[panelName]
+        panelGlobal.width = 500
+        panelGlobal.numPoints = 5
+        panelGlobal.tooltipAnchor = 'ANCHOR_BOTTOM'
+        panelGlobal.tooltipXOffset = 0
+        panelGlobal.tooltipYOffset = -4
+        panelGlobal.fonts.enable = true
+        panelGlobal.name = panelName
+
+        DT:SetupPanelOptions(panelName)
+        DT:BuildPanelFrame(panelName)
+    else
+        DT:UpdatePanelAttributes(panelName, E.global.datatexts.customPanels[panelName])
+    end
+
+    local panelMover = _G['DTPanel' .. panelName .. 'Mover']
+    panelMover:ClearAllPoints()
+    panelMover:SetPoint('TOPLEFT', E.UIParent, 'TOPLEFT', 0, 0)
+    E:SaveMoverPosition(panelMover.name)
+
+    E.db.bags.bankCombined = true
+    E.db.general.autoRepair = 'GUILD'
+    E.db.general.itemLevel.showItemLevel = false
+    E.db.chat.timeStampFormat = '%H:%M:%S '
+end
+
+local function HookSetupElvUI(_, import)
+    if import then
+        TweakElvUIProfile()
     end
 end
 
@@ -163,6 +333,8 @@ local function NaowhUI()
                 return originalClassCooldowns(addon, import, ...)
             end
         end
+
+        hooksecurefunc(SE, 'ElvUI', HookSetupElvUI)
     end)
 end
 
